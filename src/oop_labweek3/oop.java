62e5aaab9c1f7abf843d4eacf8962f0774c2243e
prakasitz <59050083@kmitl.ac.th>
2018-08-31 10:11:11 +0700

    test commit oop_week3

---
+++ src/oop_labweek3/oop.java
@@ -0,0 +1,14 @@
+package oop_labweek3;
+
+public class oop {
+	String description;
+	public oop(String description) {
+		this.description = description;
+	}
+	public void setDes(String description) {
+		this.description = description;
+	}
+	public String getDes() {
+		return description;
+	}
+}
---
+++ src/oop_labweek3/oop_test.java
@@ -0,0 +1,9 @@
+package oop_labweek3;
+
+public class oop_test {
+	public static void main(String[] args) {
+		oop OOPONE = new oop("Hello");
+		OOPONE.setDes("World");
+		System.out.println(OOPONE.getDes());
+	}
+}
